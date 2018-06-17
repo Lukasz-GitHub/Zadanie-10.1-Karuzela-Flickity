71f9f52313099b10cfc39379b6307e47b1aa2783
Lukasz Borek <borek_lukasz@outlook.com>
2018-06-18 00:33:19 +0200

    Change files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Pętla z informacjami do szablonu Mustache
 var templateList = document.getElementById('template-slide').innerHTML;
 var elem = document.querySelector('.main-carousel');
 Mustache.parse(templateList);
@@ -5,18 +6,18 @@ Mustache.parse(templateList);
 var generatedList = '';
 
 for (var i = 0; i < slideData.length; i++) {
-    console.log(slideData);
+    console.log(slideData)
     generatedList += Mustache.render(templateList, slideData[i]);
 }
 
 elem.insertAdjacentHTML('beforeend', generatedList);
-
+// Karuzela Flickity
 var flkty = new Flickity(elem, {
     cellAlign: 'left',
     contain: true,
     pageDots: false
 });
-
+// Przycisk powrotu do pierwszego slajcu
 var buttonReset = document.querySelector('.button-reset');
 var button = buttonReset.querySelector('.button');
 button = fizzyUIUtils.makeArray(button);
@@ -28,9 +29,23 @@ buttonReset.addEventListener('click', function (event) {
     var index = button.indexOf(event.target);
     flkty.select(index);
 });
-
+// Pasek postępu
 var progressBar = document.querySelector('.progress-bar')
 flkty.on('scroll', function (progress) {
     progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress * 100 + '%';
 });
+// Mapa
+window.initMap = function () {
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 4,
+            center: slideData[0].coords
+        });
+    for (var i = 0; i < slideData.length; i++) {
+        var marker = new google.maps.Marker({
+            position: slideData[i].coords,
+            map: map
+        });
+    }
+}
