cb00e78859b4e60a351c0e7943abf8efe5986760
Lukasz Borek <borek_lukasz@outlook.com>
2018-06-19 20:41:46 +0200

    Change script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Pętla z informacjami do szablonu Mustache
+var map;
 var templateList = document.getElementById('template-slide').innerHTML;
 var elem = document.querySelector('.main-carousel');
 Mustache.parse(templateList);
@@ -18,36 +19,40 @@ var flkty = new Flickity(elem, {
 });
 // Przycisk powrotu do pierwszego slajdu
 var buttonReset = document.querySelector('.button-reset');
-var button = buttonReset.querySelector('.button');
-button = fizzyUIUtils.makeArray(button);
-
 buttonReset.addEventListener('click', function (event) {
-    if (!matchesSelector(event.target, '.button')) {
-        return;
-    }
-    var index = button.indexOf(event.target);
-    flkty.select(index);
+    flkty.select(0);
 });
+
 // Pasek postępu
 var progressBar = document.querySelector('.progress-bar')
 flkty.on('scroll', function (progress) {
     progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress * 100 + '%';
 });
+
+flkty.on('change', function (index) {
+    map.setCenter(slideData[index].coords);
+    map.setZoom(6);
+});
+
 // Mapa
 window.initMap = function () {
-    var map = new google.maps.Map(
+    map = new google.maps.Map(
         document.getElementById('map'), {
-            zoom: 4,
+            zoom: 2,
             center: slideData[0].coords
-        });
+        }
+    );
+
     for (var i = 0; i < slideData.length; i++) {
         var marker = new google.maps.Marker({
             position: slideData[i].coords,
             map: map
         });
-        marker.addListener('click', function(){
-            flkty.select(index);
-        });
+
+        marker.addListener('click', function (i) {
+            zoom: 12,
+            flkty.select(i);
+        }.bind(null, i));
     }
 }
